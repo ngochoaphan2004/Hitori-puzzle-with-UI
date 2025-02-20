ab36eec6b09b738defb0cbe3ad393927691ab5bc
ngochoaphan2004 <phanngochoa2004@gmail.com>
2025-02-20 08:23:48 +0700

    fix main.py input

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from searching import Searching
 
 if __name__ == "__main__":
-    temp = Searching()
+    size = int(input("size of test:"))
+    test = int(input("test number:"))
+    temp = Searching(size, test)
     choose = int(input("chose bfs: 1 or aStar: 2 --> "))
     if choose == 1:
         print("BFS solution: ")
